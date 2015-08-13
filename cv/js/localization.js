b75d8e63d1bea80f359332d0495099e23815c521
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2015-08-14 00:06:12 +0300

    localization v1.7

--- cv/js/localization.js
+++ cv/js/localization.js
@@ -1,12 +1,16 @@
 function BindStrings(language)
 {
-	console.log("BindStrings();");
+	console.log("BindStrings() called.");
 
 	$.getJSON("./strings/" + language + ".json", function(data)
 	{
+		console.log("data : " + data);
+
 		$.each(data, function(key, val)
 		{
 			console.log(key + " : " + val);
 		});
 	});
+
+	console.log("BindStrings() finished.");
 }
