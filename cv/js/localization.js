71c22087d8e23884e862ee40578c9e31d3a21c7e
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2015-08-14 00:26:30 +0300

    localization v2.0
    Json verisi okundu.

--- cv/js/localization.js
+++ cv/js/localization.js
@@ -1,11 +1,7 @@
 function BindStrings(language)
 {
-	console.log("BindStrings() called.");
-
 	$.getJSON("./strings/" + language + ".json", function(data)
 	{
-		console.log("data : " + data);
-
 		$.each(data, function(key, val)
 		{
 			console.log(key + " : " + val);
@@ -13,18 +9,14 @@ function BindStrings(language)
 	})
 	.done(function()
 	{
-		console.log("done();");
+		console.log("Json verisi basariyla okundu.");
 	})
 	.fail(function(jqxhr, textStatus, error)
 	{
-		console.log("fail();");
+		console.log("Json hatasi!");
 		var errMessage = textStatus + ", " + error;
-		console.log( "Request Failed: " + errMessage );
-	})
-	.always(function()
-	{
-		console.log("always();");
+		console.log( "Hata : " + errMessage );
 	});
 
-	console.log("BindStrings() finished.");
+	console.log("String veriler bind edildi.");
 }
