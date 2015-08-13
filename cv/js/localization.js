a4e7c48ae6181d634614fc44c916ca1a5fd40453
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2015-08-13 23:48:33 +0300

    localization v1.1

--- cv/js/localization.js
+++ cv/js/localization.js
@@ -2,9 +2,9 @@ function BindStrings(language)
 {
 	//var json = ReadFromFile("/strings/" + language + ".json");
 
-	$.getJSON("./strings/" + language + ".json", function(json)
+	$.getJSON("/strings/" + language + ".json", function(json)
 	{
-		alert(json);
+		console.log(json);
 	});
 }
 
