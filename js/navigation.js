528dd7083cdbd8814e010885de9d6e32c127e97d
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2015-08-20 14:38:15 +0300

    Sayfa navigasyon özelliği eklendi

---
+++ js/navigation.js
@@ -0,0 +1,20 @@
+﻿function navigate(page, menuitem)
+{
+    $("#ContentDiv").load(page);
+
+    if (menuitem != null)
+        doSelected(menuitem);
+};
+
+function doSelected(element)
+{
+    if (element.className == 'selected')
+        return;
+
+    // Eski selected elemanın unselected yapılması
+    var menuItems = document.getElementsByClassName("selected");
+    menuItems[0].className = 'unselected';
+
+    // Yeni elemanın selected yapılması
+    element.className = 'selected';
+};
