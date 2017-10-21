0770b37c3021fb9f166dd7f14a681afe03744b70
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2017-10-22 02:21:04 +0300

    Response messages fixed

--- ertis/js/ertis/login.js
+++ ertis/js/ertis/login.js
@@ -13,14 +13,21 @@ function Login(username, password)
 	var httpReq = new XMLHttpRequest();
 	httpReq.onreadystatechange = function ()
 	{
+		if (!this.responseText)
+			return;
+		
 		if (this.readyState == 4 && this.status == 200)
 		{
 			showMessage(this.responseText);
 		}
-		else
+		else if (this.status == 401)
 		{
+			showErrorMessage("Kullanıcı adı ya da şifre hatalı!");
+		}
+		else
+		{	
 			showErrorMessage(this.responseText);
-		}	
+		}
 	};
 
 	httpReq.open("POST", SERVICE_URL, true);
@@ -28,42 +35,6 @@ function Login(username, password)
 	httpReq.setRequestHeader('Access-Control-Allow-Origin', '*');
 	httpReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	httpReq.send("username=" + username + "&password=" + password);
-}
-
-function LoginWithAjax(username, password)
-{
-	$.ajax(
-	{	
-		type: "POST",
-		url: SERVICE_URL,
-		data: $.param(
-		{
-			"username": username,
-			"password": password
-		}),
-		headers:
-		{
-			'Content-Type': 'application/x-www-form-urlencoded'
-		},
-		success: function (data)
-		{
-			//alert(data);
-			if (data.msg != null)
-			{
-				showMessage(data.msg);
-			}
-			else
-			{
-				showErrorMessage("Response data is null");
-			}
-		},
-		error: function (data)
-		{
-			showErrorMessage(data);
-		},
-		crossDomain: true,
-		dataType: 'jsonp',
-	});
 }
 
 function showMessage(message)
