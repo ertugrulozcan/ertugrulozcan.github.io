1e27913ec22da5517b67219f1a26c78b7cd992f9
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2017-10-21 23:55:15 +0300

    Ertis develop&test

---
+++ ertis/js/ertis/login.js
@@ -0,0 +1,43 @@
+var SERVICE_URL = "http://astroturf.azurewebsites.net/api/login";
+
+function LoginButtonClickHandler()
+{
+	var username = $('#usernameTextField').val();
+	var password = $('#passwordTextField').val();
+
+	this.Login(username, password);
+}
+
+function Login(username, password)
+{
+	$.ajax(
+	{	
+		type: "POST",
+		url: SERVICE_URL,
+		data: $.param(
+		{
+			"username": username,
+			"password": password
+		}),
+		headers:
+		{
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		success: function (data)
+		{
+			//alert(data);
+			if (data.msg != null)
+			{
+				alert(data.msg);
+			}
+			else
+			{
+				alert("anan");
+			}
+		},
+		error: function (data)
+		{
+			alert(data);
+		}	
+	});
+}
