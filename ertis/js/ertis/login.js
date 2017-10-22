b6c5a8cc7053edd6a7b16200c9b0068b216c1154
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2017-10-23 00:33:12 +0300

    Register page added

--- ertis/js/ertis/login.js
+++ ertis/js/ertis/login.js
@@ -75,8 +75,9 @@ function Login(username, password)
 		{
 			if (this.status == 200)
 			{
-				showMessage(this.responseText);
 				console.log("Login success");
+				createCookie("access_token", JSON.stringify(this.responseText), 1);
+				window.location.assign("index.html");
 			}
 			else if (this.status == 401)
 			{
@@ -103,6 +104,34 @@ function Login(username, password)
 	httpReq.send("username=" + username + "&password=" + password);
 
 	console.log("Login request sent.");
+}
+
+function createCookie(name, value, days) {
+	var expires;
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		expires = "; expires=" + date.toGMTString();
+	}
+	else {
+		expires = "";
+	}
+	document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(c_name) {
+	if (document.cookie.length > 0) {
+		c_start = document.cookie.indexOf(c_name + "=");
+		if (c_start != -1) {
+			c_start = c_start + c_name.length + 1;
+			c_end = document.cookie.indexOf(";", c_start);
+			if (c_end == -1) {
+				c_end = document.cookie.length;
+			}
+			return unescape(document.cookie.substring(c_start, c_end));
+		}
+	}
+	return "";
 }
 
 function showMessage(message)
