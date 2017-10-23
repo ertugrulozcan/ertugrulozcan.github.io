8c4a48e49ac18d17c9843ac257485bf9dbe9f9fd
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2017-10-24 00:30:38 +0300

    New localization function test

--- ertis/js/localization.js
+++ ertis/js/localization.js
@@ -4,19 +4,24 @@ var currentDictionary = null;
 
 function FetchLocalizationDictionary(culture)
 {
-	if (window.XMLHttpRequest) {
+	if (window.XMLHttpRequest)
+	{
 		// code for modern browsers
 		xmlhttp = new XMLHttpRequest();
 	}
-	else {
+	else
+	{
 		// code for old IE browsers
 		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 	}
 
 	var httpReq = xmlhttp;
-	httpReq.onreadystatechange = function () {
-		if (this.readyState == 4) {
-			if (this.status == 200) {
+	httpReq.onreadystatechange = function ()
+	{
+		if (this.readyState == 4)
+		{
+			if (this.status == 200)
+			{
 				console.log("LocalizationData fetched.");
 				SetCurrentDictionary(JSON.parse(this.responseText));
 			}
@@ -33,7 +38,7 @@ function FetchLocalizationDictionary(culture)
 function SetCurrentDictionary(dict)
 {
 	currentDictionary = dict;
-	BindStrings(currentDictionary.KeyValueDictionary);
+	Loc(currentDictionary.KeyValueDictionary);
 }
 
 function Localize(culture)
@@ -43,9 +48,12 @@ function Localize(culture)
 	FetchLocalizationDictionary(culture);
 }
 
-function BindStrings(data) {
-	$.each(data, function (key, val) {
-		try {
+function BindStrings(data)
+{
+	$.each(data, function (key, val)
+	{
+		try
+		{
 			elements = document.getElementsByName(key);
 			for (var i = 0; i < elements.length; i++)
 			{
@@ -55,10 +63,74 @@ function BindStrings(data) {
 					elements[i].innerHTML = val;
 			}
 		}
-		catch (exc) {
+		catch (exc)
+		{
 			console.log("Hata : " + exc.message + "(key:" + key + ", value:" + val + ")");
 		}
 	});
 	
 	console.log("String veriler bind edildi.");
 }
+
+function Loc(dict)
+{
+	Loc(dict, "*");
+}
+
+function Loc(dict, tagName)
+{
+	var element, lockey, val;
+	var allElements = document.getElementsByTagName(tagName);
+
+	for (i = 0; i < allElements.length; i++)
+	{
+		element = allElements[i];
+		lockey = element.getAttribute("lockey");
+
+		if (lockey)
+		{
+			val = dict[lockey];
+
+			if (val)
+			{
+				element.innerHTML = val;
+			}
+			else
+			{
+				element.innerHTML = "Key : " + lockey;
+			}	
+			
+			// element.removeAttribute("lockey");
+		}	
+	}	
+}
+
+function Anan()
+{
+	var z, i, elmnt, file, xhttp;
+	
+	z = document.getElementsByTagName("*");
+	
+	for (i = 0; i < z.length; i++)
+	{
+		elmnt = z[i];
+		file = elmnt.getAttribute("w3-include-html");
+		
+		if (file)
+		{
+			xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function ()
+			{
+				if (this.readyState == 4 && this.status == 200)
+				{
+					elmnt.innerHTML = this.responseText;
+					elmnt.removeAttribute("w3-include-html");
+					w3.includeHTML(cb);
+				}
+			}
+			xhttp.open("GET", file, true);
+			xhttp.send();
+			return;
+		}
+	}
+};
