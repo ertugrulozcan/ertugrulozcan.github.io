27468830ad8192f90d13707f90961a82cb24c8e6
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2017-10-23 23:18:58 +0300

    Index pages seperated

--- ertis/js/ertis/index.js
+++ ertis/js/ertis/index.js
@@ -9,4 +9,19 @@ function IncludePages()
 {
 	// SideBar
 	$.get("sidebar.html", function (data) { $("#sidebar").html(data); });
+
+	// HeaderBar
+	$.get("header.html", function (data) { $("#headerbar").html(data); });
+
+	// Dashboard
+	$.get("dashboard.html", function (data) { $("#dashboard").html(data); });
+
+	// Content
+	$.get("content.html", function (data) { $("#content").html(data); });
+
+	// ChatBox
+	$.get("chatbox.html", function (data) { $("#chatbox").html(data); });
+
+	// RightSidebar
+	$.get("rightsidebar.html", function (data) { $("#rightsidebar").html(data); });
 }
