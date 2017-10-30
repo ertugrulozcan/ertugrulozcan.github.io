9147a0850c92048514de2f3d33e8f4bd39a51f30
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2017-10-31 01:04:24 +0300

    Some bugs fixed

--- ertis/js/ertis/session.js
+++ ertis/js/ertis/session.js
@@ -3,10 +3,29 @@
 //
 var session;
 
-function CreateSession(loginResponse) {
-    console.log(loginResponse);
+// Sample User Data;
+/*
+{
+    "id": 1014,
+    "card": {
+        "name": "Ismet Acar",
+        "surname": "Dogan",
+        "fullName": "Ismet Acar Dogan",
+        "birthDate": "1991-03-12T00:00:00",
+        "joinDate": "2017-10-29T22:13:38",
+        "emailAddress": "ismetacar@projectertis.com",
+        "phoneNumber": "534 611 8505"
+    },
+    "userRole": 0
+}
+*/
 
-    session = JSON.parse(loginResponse);
+function CreateSession(loginResponse) {
+    var response = JSON.parse(loginResponse);
+
+    session.token = response.access_token;
+    session.expire = response.expires_in;
+    session.userID = response.user_id;
 
     createCookie("access_token", JSON.stringify(this.responseText), 1);
     createCookie("username", username, 1);
@@ -19,4 +38,33 @@ function checkUserToken() {
     }
 
     return true;
+}
+
+function GetUser(userID) {
+    if (window.XMLHttpRequest) {
+        // code for modern browsers
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for old IE browsers
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    var httpReq = xmlhttp;
+    httpReq.onreadystatechange = function() {
+        if (!this.responseText)
+            return;
+
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                session.user = JSON.parse(this.responseText);
+            }
+        }
+    };
+
+    httpReq.open("GET", "http://ertis.azurewebsites.net/api/users/" + userID, true);
+    httpReq.setRequestHeader('Access-Control-Allow-Headers', '*');
+    httpReq.setRequestHeader('Access-Control-Allow-Origin', '*');
+    httpReq.setRequestHeader("Content-type", "application/json");
+    httpReq.setRequestHeader("Authorization", "Bearer " + session.token);
+    httpReq.send();
 }
