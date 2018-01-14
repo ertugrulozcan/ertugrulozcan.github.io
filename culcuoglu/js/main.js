8e3874d4d8546da6a05a6fa1f3f41ae135c0718b
Ahmet Ertuğrul Özcan <ertugrul.ozcan@bil.omu.edu.tr>
2018-01-15 01:39:53 +0300

    Culcuoglu redesigned

--- culcuoglu/js/main.js
+++ culcuoglu/js/main.js
@@ -1,13 +1,12 @@
-"use strict";
 /*
  * ----------------------------------------------------------------------------------------
  Author       : Iniciativa_develop
- Template Name: Theme-simon - Multipurpose
+ Template Name: Theme-Iniciativa - Multipurpose
  Version      : 1.0
  * ----------------------------------------------------------------------------------------
  */
-
 $(document).ready(function() {
+
 
   /*
    * ----------------------------------------------------------------------------------------
@@ -19,195 +18,89 @@ $(document).ready(function() {
 
   /*
    * ----------------------------------------------------------------------------------------
-   *  Revolution Slider JS
+   *  SLIDER JS
    * ----------------------------------------------------------------------------------------
    */
-  var header, bannerRevSlider;
-  // Home slider height fix
-  function homeSliderHeightFix() {
-    $('#home').height($(window).height());
-  }
 
-  var simonRevSlider = $('[data-action="simon_revslider"]'),
-    simonRevSliderArrow = {
-      style: '',
-      enable: false,
-    };
-
-  var simonRevSliderBullet = {
-    enable: true
-  };
-
-  simonRevSlider.each(function() {
-
-    var revSliderWrapID = $(this);
-
-    if (revSliderWrapID.attr('id') == 'simon_revslider_1') {
-      simonRevSliderArrow = {
-        style: 'simon-revslider-1-arrow',
-        enable: true,
-      }
-    }
-    if (revSliderWrapID.attr('id') == 'simon_revslider_2') {
-      simonRevSliderArrow = {
-        style: 'simon-revslider-2-arrow',
-        enable: true,
-      }
-    }
-    if (revSliderWrapID.attr('id') == 'simon_revslider_3') {
-      simonRevSliderBullet = {
-        enable: true,
-      }
-    }
-
-    if (revSliderWrapID.revolution == undefined) {
-      revslider_showDoubleJqueryError(revSliderWrapID);
-    } else {
-
-      bannerRevSlider = revSliderWrapID.show().revolution({
-        sliderType: "standard",
-        jsFileLocation: "assets/libs/revolution/js/",
-        sliderLayout: "fullscreen",
-        stopLoop: "off",
-        stopAfterLoops: 0, //-1
-        stopAtSlide: 1,
-        responsiveLevels: [1240, 1024, 778, 485],
-        gridwidth: [1400, 1170, 778, 480],
-        gridheight: [800, 700, 1050, 500],
-        delay: 6000,
-        shuffle: "on",
-
-        navigation: {
-          keyboardNavigation: "on",
-          onHoverStop: "off",
-          arrows: {
-            style: simonRevSliderArrow.style,
-            enable: simonRevSliderArrow.enable,
-            rtl: false,
-            hide_onmobile: false,
-            hide_onleave: true,
-            hide_delay: 200,
-            hide_delay_mobile: 1200,
-            hide_under: 0,
-            hide_over: 9999,
-            tmp: '',
-            left: {
-              container: "slider",
-              h_align: "left",
-              v_align: "center",
-              h_offset: 30,
-              v_offset: 0
-            },
-            right: {
-              container: "slider",
-              h_align: "right",
-              v_align: "center",
-              h_offset: 30,
-              v_offset: 0
-            }
-          },
-          bullets: {
-            style: "",
-            enable: simonRevSliderBullet.enable,
-            hide_onmobile: false,
-            hide_onleave: false,
-            hide_delay: 200,
-            hide_delay_mobile: 1200,
-            hide_under: 0,
-            hide_over: 9999,
-            tmp: '<span class="tp-bullet-image"></span><span class="tp-bullet-title"></span>',
-            direction: "horizontal",
-            space: 10,
+  if ($("#rev_slider_70_1").revolution === undefined) {
+    revslider_showDoubleJqueryError("#rev_slider_70_1");
+  } else {
+    $('#homeSection').height($(window).height());
+    $("#rev_slider_70_1").show().revolution({
+      sliderType: "standard",
+      jsFileLocation: "../libs/assets/libs/revolution/js/",
+      sliderLayout: "fullscreen",
+      stopLoop: 'off',
+      stopAfterLoops: -1, //-1
+      stopAtSlide: 1,
+      responsiveLevels: [1200, 1024, 768, 480],
+      gridwidth: [1200, 1050, 768, 480],
+      gridheight: [800, 700, 1050, 500],
+      delay: 5000,
+      shuffle: "off",
+      navigation: {
+        keyboardNavigation: "on",
+        onHoverStop: "on",
+        arrows: {
+          style: "",
+          enable: true,
+          rtl: false,
+          hide_onmobile: false,
+          hide_onleave: false,
+          hide_delay: 200,
+          hide_delay_mobile: 1200,
+          hide_under: 0,
+          hide_over: 9999,
+          tmp: '',
+          left: {
+            container: "slider",
             h_align: "center",
-            v_align: "middle",
-            h_offset: 0,
-            v_offset: 50
+            v_align: "bottom",
+            h_offset: -25,
+            v_offset: 20
+          },
+          right: {
+            container: "slider",
+            h_align: "center",
+            v_align: "bottom",
+            h_offset: 25,
+            v_offset: 20
           }
-        },
-
-        lazyType: "none",
-        parallax: {
-          type: "mouse+scroll",
-          origo: "slidercenter",
-          speed: 2000,
-          levels: [1, 2, 3, 20, 25, 30, 35, 40, 45, 50],
-          disable_onmobile: "on"
-        },
-        shadow: 0,
-        spinner: "spinner2",
-        autoHeight: "on",
-        disableProgressBar: "off",
-        hideThumbsOnMobile: "off",
-        hideSliderAtLimit: 0,
-        hideCaptionAtLimit: 0,
-        hideAllCaptionAtLilmit: 0,
-        debugMode: false,
-        fallbacks: {
-          simplifyAll: "off",
-          nextSlideOnWindowFocus: 'off',
-          disableFocusListener: false
         }
-      });
-      revSliderWrapID.bind("revolution.slide.onchange", function(e, data) {
-        if (data.currentslide.hasClass('slider-color-schema-white')) {
-          $('body').removeClass('color-schema-white').addClass('color-schema-dark');
-        } else if (data.currentslide.hasClass('slider-color-schema-dark')) {
-          $('body').removeClass('color-schema-dark').addClass('color-schema-white');
-        }
-      });
-    }
-  });
-  // Home slider height fix
-  homeSliderHeightFix();
-
-  /*
-   * ----------------------------------------------------------------------------------------
-   *  MENU RESPONSIVE JS
-   * ----------------------------------------------------------------------------------------
-   */
-  $(function() {
-    var ancho = $(window).width(),
-      links = $('#links'),
-      btnMenu = $('#btn-menu'),
-      icono = $('#btn-menu .icono');
-    if (ancho < 768) {
-      links.hide();
-      icono.addClass('ion-navicon-round');
-    }
-    btnMenu.on('click', function() {
-      links.slideToggle();
-      icono.toggleClass('ion-navicon-round');
-      icono.toggleClass('ion-close-round');
+      },
+      lazyType: "none",
+      parallax: {
+        type: "mouse+scroll",
+        origo: "slidercenter",
+        speed: 2000,
+        levels: [1, 2, 3, 20, 25, 30, 35, 40, 45, 50],
+        disable_onmobile: "on"
+      },
+      shadow: 0,
+      spinner: "spinner2",
+      autoHeight: "on",
+      disableProgressBar: "off",
+      hideThumbsOnMobile: "off",
+      hideSliderAtLimit: 0,
+      hideCaptionAtLimit: 0,
+      hideAllCaptionAtLilmit: 0,
+      debugMode: false,
     });
-    $(window).on('resize', function() {
-      if ($(this).width() > 768) {
-        links.show();
-        icono.addClass('ion-navicon-round');
-        icono.removeClass('ion-close-round');
-      } else {
-        links.hide();
-      }
-    });
-  });
+  }
 
   /*
    * ----------------------------------------------------------------------------------------
    *  CHANGE MENU BACKGROUND AND TO UP JS
    * ----------------------------------------------------------------------------------------
    */
-
   $(window).scroll(function() {
-    var scroll = $(window).scrollTop(),
-      header = $('.main-header'),
-      button = $('#backToTop');
-    if (scroll > 250) {
-      header.addClass('is-sticky');
-      button.fadeIn();
-      button.addClass('visible');
+    var header = $('.main-header');
+    if ($(this).scrollTop() > 350) {
+      header.addClass('main-header-top');
+      $('#backToTop').fadeIn();
     } else {
-      header.removeClass('is-sticky');
-      button.fadeOut();
-      button.removeClass('visible');
+      header.removeClass('main-header-top');
+      $('#backToTop').fadeOut();
     }
   });
 
@@ -226,119 +119,182 @@ $(document).ready(function() {
   $('nav a.page-scroll').on("click", function(e) {
     var anchor = $(this);
     $('html, body').stop().animate({
-      scrollTop: $(anchor.attr('href')).offset().top - 81
+      scrollTop: $(anchor.attr('href')).offset().top - 66
     }, 1000);
     e.preventDefault();
   });
-
-  /*
-   * ----------------------------------------------------------------------------------------
-   *  TABS JS
-   * ----------------------------------------------------------------------------------------
-   */
-
-  $(function() {
-    $('ul.nav-tabs li a:first').addClass('active');
-    $('.tab-content div').hide();
-    $('.tab-content div:first').show();
-    $('ul.nav-tabs li a').on("click", function() {
-      $('ul.nav-tabs li a').removeClass('active');
-      $(this).addClass('active');
-      $('.tab-content div').hide();
-      var activeTab = $(this).attr('href');
-      $(activeTab).fadeIn('slow');
-      return false;
-    });
-
+  $('#header__icon').on("click", function(e) {
+    var header_icon = $(this);
+    $('body').toggleClass('with--sidebar');
+    if ($(this).hasClass('header__icon__close')) {
+      header_icon.removeClass('header__icon__close');
+    } else {
+      header_icon.addClass('header__icon__close');
+    }
+    e.preventDefault();
   });
-
   /*
    * ----------------------------------------------------------------------------------------
    *  USER & SCREENSHOTS JS
    * ----------------------------------------------------------------------------------------
    */
+  // carousel
+  $("#owl-carousel-client").owlCarousel({
+    loop: true,
+    margin: 35,
+    nav: true,
+    dots: false,
+    navText: ["&#xf053", "&#xf054"],
+    pag: false,
+    responsive: {
+      320: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      1024: {
+        items: 3
+      },
+      1366: {
+        items: 5
+      },
+      1440: {
+        items: 5
+      }
+    }
+  });
+  // carousel
+  $("#owl-carousel-comment").owlCarousel({
+    loop: true,
+    margin: 35,
+    nav: true,
+    dots: false,
+    navText: ["&#xf053", "&#xf054"],
+    pag: false,
+    responsive: {
+      320: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      768: {
+        items: 1
+      },
+      1024: {
+        items: 1
+      },
+      1366: {
+        items: 1
+      },
+      1440: {
+        items: 1
+      }
+    }
+  });
 
-   // carousel
-   $("#client-slider").owlCarousel({
-     loop: true,
-     center: false,
-     margin: 35,
-     nav: false,
-     dots: false,
-     navText: ["&#xf124", "&#xf125"],
-     pag:false,
-     responsive: {
-       320: { items: 2 },
-       600: { items: 2 },
-       768: { items: 3 },
-       1024: { items: 5 },
-       1366: { items: 5 },
-       1440: { items: 5 }
-     }
-   });
+  $("#owl-carousel-team").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: false,
+    navText: ["&#xf053", "&#xf054"],
+    pag: false,
+    responsive: {
+      320: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      1024: {
+        items: 2
+      },
+      1366: {
+        items: 3
+      },
+      1440: {
+        items: 3
+      }
+    }
+  });
 
-   // carousel
-   $("#single-carousel").owlCarousel({
-     loop: true,
-     center: false,
-     margin: 35,
-     nav: false,
-     dots: true,
-     navText: ["&#xf124", "&#xf125"],
-     pag:true,
-     responsive: {
-       320: { items: 1 },
-       600: { items: 1 },
-       768: { items: 1 },
-       1024: { items: 1 },
-       1366: { items: 1 },
-       1440: { items: 1 }
-     }
-   });
+  $("#owl-carousel-portfolio").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: false,
+    navText: ["&#xf053", "&#xf054"],
+    pag: false,
+    responsive: {
+      320: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      768: {
+        items: 1
+      },
+      1024: {
+        items: 1
+      },
+      1366: {
+        items: 1
+      },
+      1440: {
+        items: 1
+      }
+    }
+  });
 
-   // carousel
-   $("#team-carousel").owlCarousel({
-     loop: true,
-     center: false,
-     margin: 35,
-     nav: false,
-     dots: false,
-     navText: ["&#xf124", "&#xf125"],
-     pag:false,
-     responsive: {
-       320: { items: 1 },
-       600: { items: 2 },
-       768: { items: 2 },
-       1024: { items: 3 },
-       1366: { items: 4 },
-       1440: { items: 4 }
-     }
-   });
+  /*
+   * ----------------------------------------------------------------------------------------
+   *  ISOTOPE JS
+   * ----------------------------------------------------------------------------------------
+   */
+  // activate isotope in container
+  var $grid = $('#portfolio_items').isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows'
+  });
+  /*
+    $('#portfolio_items').isotope({
+      layoutMode: 'masonry',
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      masonry: {
+        columnWidth: '.grid-sizer'
+      }
+    });*/
+  // add isotope click function
+  $('#portfolio_filter li').on("click", function() {
+    var selector = $(this).attr('data-filter');
+    $('#portfolio_items').isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 750,
+        easing: 'linear',
+        queue: false
+      }
+    });
 
-   // carousel
-   $("#blog-carousel").owlCarousel({
-     loop: true,
-     center: false,
-     margin: 25,
-     nav: false,
-     dots: false,
-     navText: ["&#xf124", "&#xf125"],
-     pag:false,
-     responsive: {
-       320: { items: 1 },
-       600: { items: 1 },
-       768: { items: 1 },
-       1024: { items: 2 },
-       1366: { items: 3 },
-       1440: { items: 3 }
-     }
-   });
+    // change active class on li
+    $('#portfolio_filter ul li').each(function() {
+      var $portfolio_filter = $('#portfolio_filter ul li');
+      $portfolio_filter.removeClass('active');
+      $portfolio_filter.on('click', function() {
+        $(this).addClass('active');
+      });
+    });
+    return false;
+  });
 
-   $('.jquery_accordion_title').on("click", function() {
-     $(this).closest('.jquery_accordion_item').siblings().removeClass('in').find('.jquery_accordion_content').slideUp(400);
-   	$(this).closest('.jquery_accordion_item').siblings().removeClass('active').find('.jquery_accordion_content').slideUp(400);
-   	$(this).closest('.jquery_accordion_item').toggleClass('active').find('.jquery_accordion_content').slideToggle(400);
-   	return false;
-   });
-$('.dropdown-toggle').dropdown();
 });
